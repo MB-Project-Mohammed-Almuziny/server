8e162478510cabccede7dba02eda1ffac99d2aa4
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-20 14:50:18 +0300

    create and connect datebase

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 require("dotenv").config();
 
+require("./db");
+
 const app = express();
 app.use(express.json());
 app.use(cors());
