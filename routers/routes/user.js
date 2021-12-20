31aa2a240a16205f414840361c5c6dc81e8158cd
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-20 16:34:08 +0300

    create user setting route

--- routers/routes/user.js
+++ routers/routes/user.js
@@ -5,7 +5,9 @@ const { register, logIn } = require("./../controllers/users");
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
-
 userRouter.post("/login", logIn);
+userRouter.put("/:userId", (req, res) => {
+  res.send("success");
+});
 
 module.exports = userRouter;
