9994d8540185c4aff659b954f707e2bd062d1659
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-20 16:26:36 +0300

    create log in controllers

--- routers/controllers/users.js
+++ routers/controllers/users.js
@@ -1,9 +1,11 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const usersModel = require("./../../db/models/users");
 
 const SALT = Number(process.env.SALT);
+const SECRET = process.env.SECRETKEY;
 
 const register = async (req, res) => {
   try {
@@ -31,4 +33,59 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const logIn = (req, res) => {
+  try {
+    const { nameOrEmail, password } = req.body;
+
+    const savedEmail = nameOrEmail.toLowerCase();
+
+    console.log(nameOrEmail);
+
+    usersModel
+      .findOne({
+        $or: [{ name: nameOrEmail }, { email: savedEmail }],
+        isBlocked: false,
+        // isVerified: true,
+      })
+      // .populate("role")
+      .then(async (result) => {
+        if (result) {
+          if (result.email === savedEmail || result.name === nameOrEmail) {
+            const savedPassword = await bcrypt.compare(
+              password,
+              result.password
+            );
+
+            if (savedPassword || result.name === nameOrEmail) {
+              const payload = {
+                // role: result.role,
+                id: result._id,
+              };
+
+              const options = {
+                expiresIn: "9000m",
+              };
+
+              const token = await jwt.sign(payload, SECRET, options);
+
+              res.status(200).json({ result, token });
+            } else {
+              res.status(400).json({ message: "invalid email or password" });
+            }
+          } else {
+            res.status(400).json({ message: "invalid email or password" });
+          }
+        } else {
+          res.status(404).json({ message: "email dose not exist" });
+        }
+      })
+      .catch((err) => {
+        res.status(400).json({ error: err.message });
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = { register, logIn };
--- routers/routes/user.js
+++ routers/routes/user.js
@@ -1,17 +1,11 @@
 const express = require("express");
 
-const { register } = require("./../controllers/users");
+const { register, logIn } = require("./../controllers/users");
 
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
 
-userRouter.get("/login", (req, res) => {
-  try {
-    res.status(200).json("success");
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+userRouter.post("/login", logIn);
 
 module.exports = userRouter;
