8529af67777364bd6ed0ce1cf2dfaa1fc31bddc7
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-21 10:26:09 +0300

    create get roles routes

--- routers/routes/roles.js
+++ routers/routes/roles.js
@@ -5,5 +5,8 @@ const { createRole } = require("./../controllers/roles");
 const rolesRouter = express.Router();
 
 rolesRouter.post("/createRole", createRole);
+rolesRouter.get("/getRole", (req, res) => {
+  res.send("success");
+});
 
 module.exports = rolesRouter;
