93a9eaaa6a2d88fed2b02937e58891fb86227d89
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-21 10:52:46 +0300

    create authentication middleware

--- routers/routes/roles.js
+++ routers/routes/roles.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const { createRole, getRoles } = require("./../controllers/roles");
+const authentication = require("./../middlewares/authentication");
 
 const rolesRouter = express.Router();
 
-rolesRouter.post("/createRole", createRole);
-rolesRouter.get("/getRoles", getRoles);
+rolesRouter.post("/createRole", authentication, createRole);
+rolesRouter.get("/getRoles", authentication, getRoles);
 
 module.exports = rolesRouter;
--- routers/routes/users.js
+++ routers/routes/users.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const { register, logIn, setting } = require("./../controllers/users");
+const authentication = require("./../middlewares/authentication");
 
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
 userRouter.post("/login", logIn);
-userRouter.put("/:userId", setting);
+userRouter.put("/:userId", authentication, setting);
 
 module.exports = userRouter;
