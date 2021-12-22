6616cb2f2852ffc2aee8ed60522a9271228e2de9
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-22 10:07:04 +0300

    create get course by id controller

--- routers/routes/courses.js
+++ routers/routes/courses.js
@@ -12,5 +12,8 @@ const coursesRouter = express.Router();
 coursesRouter.get("/", getAllCourses);
 coursesRouter.post("/", authentication, createCourse);
 coursesRouter.get("/search/:term", coursesSearch);
+coursesRouter.get("/:userId", (req, res) => {
+  res.send("success");
+});
 
 module.exports = coursesRouter;
