c260a5be9a8f4aa361d812cc39c8ac5e69cb5cde
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-22 09:32:57 +0300

    create get all courses route

--- routers/routes/courses.js
+++ routers/routes/courses.js
@@ -5,6 +5,9 @@ const authentication = require("./../middlewares/authentication");
 
 const coursesRouter = express.Router();
 
+coursesRouter.get("/", (req, res) => {
+  res.send("success");
+});
 coursesRouter.post("/", authentication, createCourse);
 
 module.exports = coursesRouter;
