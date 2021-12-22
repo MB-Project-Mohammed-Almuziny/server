b5047215f9cdb0ac528307f48177ed798c4993b5
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-22 09:40:55 +0300

    create course search route

--- routers/routes/courses.js
+++ routers/routes/courses.js
@@ -7,5 +7,8 @@ const coursesRouter = express.Router();
 
 coursesRouter.get("/", getAllCourses);
 coursesRouter.post("/", authentication, createCourse);
+coursesRouter.get("/search/:term", (req, res) => {
+  res.send("success");
+});
 
 module.exports = coursesRouter;
