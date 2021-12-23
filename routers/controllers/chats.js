630ff84ef5203e4411084d25d7f5e4a5d8992d61
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-23 16:34:34 +0300

    create get chat by id controller

--- routers/controllers/chats.js
+++ routers/controllers/chats.js
@@ -43,7 +43,7 @@ const sendMessage = (req, res) => {
 const getUserChats = (req, res) => {
   try {
     const { userId } = req.params;
-    
+
     chatsModel
       .find({ $or: [{ user1: userId }, { user1: userId }] })
       .then((result) => {
@@ -57,4 +57,20 @@ const getUserChats = (req, res) => {
   }
 };
 
-module.exports = { sendMessage, getUserChats };
+const getChatById = (req, res) => {
+  try {
+    chatsModel
+      .findById(req.params.chatId)
+      .then((result) => {
+        if (result) res.status(200).json(result);
+        else res.status(404).json({ error: " chat not found" });
+      })
+      .catch((err) => {
+        res.status(400).json({ error: err.message });
+      });
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+};
+
+module.exports = { sendMessage, getUserChats, getChatById };
--- routers/routes/chats.js
+++ routers/routes/chats.js
@@ -1,14 +1,16 @@
 const express = require("express");
 
-const { sendMessage, getUserChats } = require("./../controllers/chats");
+const {
+  sendMessage,
+  getUserChats,
+  getChatById,
+} = require("./../controllers/chats");
 const authentication = require("./../middlewares/authentication");
 
 const chatsRouter = express.Router();
 
 chatsRouter.post("/", authentication, sendMessage);
 chatsRouter.get("/user/:userId", authentication, getUserChats);
-chatsRouter.get("/:chatId", (req, res) => {
-  res.send("success");
-});
+chatsRouter.get("/:chatId", authentication, getChatById);
 
 module.exports = chatsRouter;
