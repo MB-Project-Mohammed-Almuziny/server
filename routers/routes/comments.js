89791d59e23e7dbf22efbf297e26d735b206c433
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-23 09:33:45 +0300

    create block comment route

--- routers/routes/comments.js
+++ routers/routes/comments.js
@@ -2,10 +2,19 @@ const express = require("express");
 
 const { addcomment, getCommentById } = require("./../controllers/comments");
 const authentication = require("./../middlewares/authentication");
+const authorization = require("./../middlewares/authorization");
 
 const commentsRouter = express.Router();
 
 commentsRouter.post("/", authentication, addcomment);
+commentsRouter.put(
+  "/block/:commentId",
+  authentication,
+  authorization,
+  (req, res) => {
+    res.send("success");
+  }
+);
 commentsRouter.get("/:commentId", authentication, getCommentById);
 
 module.exports = commentsRouter;
