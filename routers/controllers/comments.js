555437de7bf6a85f997e8a2c7c8bd52cc71a5cbe
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-23 09:27:26 +0300

    create get comment by id controller

--- routers/controllers/comments.js
+++ routers/controllers/comments.js
@@ -23,4 +23,20 @@ const addcomment = (req, res) => {
   }
 };
 
-module.exports = { addcomment };
+const getCommentById = (req, res) => {
+  try {
+    commentModel
+      .find({ _id: req.params.commentId, isBocked: false })
+      .then((result) => {
+        if (result && result[0]) res.status(200).json(result);
+        else res.status(404).json({ error: " comment not found" });
+      })
+      .catch((err) => {
+        res.status(400).json({ error: err.message });
+      });
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+};
+
+module.exports = { addcomment, getCommentById };
--- routers/routes/comments.js
+++ routers/routes/comments.js
@@ -1,13 +1,11 @@
 const express = require("express");
 
-const { addcomment } = require("./../controllers/comments");
+const { addcomment, getCommentById } = require("./../controllers/comments");
 const authentication = require("./../middlewares/authentication");
 
 const commentsRouter = express.Router();
 
 commentsRouter.post("/", authentication, addcomment);
-commentsRouter.get("/:commentId", authentication, (req, res) => {
-  res.send("success");
-});
+commentsRouter.get("/:commentId", authentication, getCommentById);
 
 module.exports = commentsRouter;
