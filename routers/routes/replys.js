e6e7605ca5a2806e5f0107d37ce4c6100519d50e
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-23 10:47:51 +0300

    create block reply route

--- routers/routes/replys.js
+++ routers/routes/replys.js
@@ -2,10 +2,19 @@ const express = require("express");
 
 const { addReply, getReplyById } = require("./../controllers/replys");
 const authentication = require("./../middlewares/authentication");
+const authorization = require("./../middlewares/authorization");
 
 const replysRouter = express.Router();
 
 replysRouter.post("/", authentication, addReply);
+replysRouter.put(
+  "/block/:commentId",
+  authentication,
+  authorization,
+  (req, res) => {
+    res.send("success");
+  }
+);
 replysRouter.get("/:replyId", authentication, getReplyById);
 
 module.exports = replysRouter;
