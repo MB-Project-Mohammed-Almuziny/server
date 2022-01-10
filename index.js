a610aafca395220836ebe2eed1ee9e6ebf03a359
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2022-01-10 09:58:23 +0300

    remove socket

--- index.js
+++ index.js
@@ -33,39 +33,40 @@ app.get("/",  (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => {
+// const server = 
+app.listen(PORT, () => {
   console.log(`SERVER ON ${PORT}`);
 });
 
-const io = socket(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  },
-});
+// const io = socket(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   },
+// });
 
-io.on("connection", (socket) => {
-  console.log("socket connect");
+// io.on("connection", (socket) => {
+//   console.log("socket connect");
 
-  socket.on("join_room", (data) => {
-    socket.join(data.room);
-    console.log(`${data.userName} has entered the room number ${data.room}`);
-  });
+//   socket.on("join_room", (data) => {
+//     socket.join(data.room);
+//     console.log(`${data.userName} has entered the room number ${data.room}`);
+//   });
 
-  socket.on("send_message", (data) => {
-    chatsModel
-      .findByIdAndUpdate(
-        data.room,
-        {
-          $push: { messages: { content: data.content, sender: data.sender } },
-        },
-        { new: true }
-      )
-      .then((result) => {
-        io.to(data.room).emit("recieve_message", result.messages);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
-});
+//   socket.on("send_message", (data) => {
+//     chatsModel
+//       .findByIdAndUpdate(
+//         data.room,
+//         {
+//           $push: { messages: { content: data.content, sender: data.sender } },
+//         },
+//         { new: true }
+//       )
+//       .then((result) => {
+//         io.to(data.room).emit("recieve_message", result.messages);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   });
+// });
